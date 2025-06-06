1e7b47ce3e28e53054b2478a0b81046533147965
urkiddennn <154726035+urkiddennn@users.noreply.github.com>
2025-06-06 16:03:06 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/google/generative-ai-go/genai"
+	"github.com/joho/godotenv"
+	"github.com/pterm/pterm"
+	"google.golang.org/api/option"
+)
+
+func loadEnv() string {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	API_KEY := os.Getenv("GEMINI_API_KEY")
+
+	return API_KEY
+}
+
+func main() {
+	GEMINI_API_KEY := loadEnv()
+
+	inputUser := userInput()
+
+	ctx := context.Background()
+	client, err := genai.NewClient(ctx, option.WithAPIKey(GEMINI_API_KEY))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+
+	model := client.GenerativeModel("gemini-1.5-flash")
+
+	response, err := model.GenerateContent(ctx, genai.Text(inputUser))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	printResponse(response)
+}
+
+func userInput() string {
+	result, _ := pterm.DefaultInteractiveTextInput.Show()
+
+	return result
+}
+
+func printResponse(resp *genai.GenerateContentResponse) {
+	for _, cand := range resp.Candidates {
+		if cand.Content != nil {
+			for _, part := range cand.Content.Parts {
+				fmt.Println(part)
+			}
+		}
+	}
+	fmt.Println("---")
+}
